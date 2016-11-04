6f5b2cbfcf64473e30c2aa37fa01e87c015e594e
Gaston <gnp.gaston@gmail.com>
2016-11-04 17:54:17 -0300

    script que elimina las stopwords del listado de palabras

--- scripts/allwords.py
+++ scripts/allwords.py
@@ -18,7 +18,6 @@ from nltk.corpus import stopwords
 csv.field_size_limit(sys.maxsize)
     
 #Elimina el codigo html dentro de las reviews
-
 def cleanhtml(raw_html):
 	cleanr =re.compile('<.*?>')
 	cleantext = re.sub(cleanr,'', raw_html)
@@ -29,7 +28,6 @@ def countWords(line):
 	text = cleanhtml(line)
 	nonPunct = re.compile(".*[A-Za-z0-9].*")
 	filtered = [w.lower().encode('utf-8') for w in TweetTokenizer().tokenize(text) if nonPunct.match(w)]
-	#return Counter(filtered)
 	return filtered
     
 def saveWords():
@@ -39,15 +37,13 @@ def saveWords():
 
 
 count = 0
-words = {}#Counter({''})
+words = {}
 
 with open('../../Data/train.csv', 'r') as csvfile1:
 	spamreader = csv.reader(csvfile1)
 	for rowlist in spamreader:
 		if(count % 1000 == 0):
 			print(count)
-		#if(count > 0):
-			#words = words + countWords(rowlist[9])
 		if(count > 0):
 			for w in countWords(rowlist[9]):
 				if w in words:			
